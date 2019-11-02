ed587e453556119fb2c45ac3e97115f635f841ab
feldbacherSimone <simone.feldbacher03@htl-salzburg.ac.at>
2019-11-02 20:56:53 +0100

    added Debug log in script

--- Assets/MyGame/Scripts/HelloWorldHome.cs
+++ Assets/MyGame/Scripts/HelloWorldHome.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello World in hell");
     }
 
     // Update is called once per frame
